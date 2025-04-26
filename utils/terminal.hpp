b86054da2e5dab0d7c7fe5aa8b4452fb5f0ec7b7
puduk <demirmert986@gmail.com>
2025-04-26 22:24:04 +0200

    Add terminal utilities (terminal.hpp)

---
+++ utils/terminal.hpp
@@ -0,0 +1,13 @@
+#ifndef TERMINAL_HPP
+#define TERMINAL_HPP
+
+#define reset_color "\033[0m"
+#define red "\033[31m"
+#define green "\033[32m"
+#define yellow "\033[33m"
+#define blue "\033[34m"
+#define magenta "\033[35m"
+#define cyan "\033[36m"
+#define white "\033[37m"
+
+#endif
